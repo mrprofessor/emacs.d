d2b258e04c737b26c0605597538a08565a412b58
mrprofessor <contact@rudra.dev>
2022-08-16 14:03:31 +0530

    Link files

--- init.el
+++ init.el
@@ -1,20 +1,2 @@
 (require 'org)
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
-
-
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("28a104f642d09d3e5c62ce3464ea2c143b9130167282ea97ddcc3607b381823f" "f490984d405f1a97418a92f478218b8e4bcc188cf353e5dd5d5acd2f8efd0790" "2d035eb93f92384d11f18ed00930e5cc9964281915689fa035719cab71766a15" "e1f4f0158cd5a01a9d96f1f7cdcca8d6724d7d33267623cc433fe1c196848554" "4fda8201465755b403a33e385cf0f75eeec31ca8893199266a6aeccb4adedfa4" "70b596389eac21ab7f6f7eb1cf60f8e60ad7c34ead1f0244a577b1810e87e58c" "5b9a45080feaedc7820894ebbfe4f8251e13b66654ac4394cb416fef9fdca789" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" default))
- '(package-selected-packages
-   '(all-the-icons doom-modeline toc-org org-tempo org-modern org-bullets doom-themes evil-collection evil use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
---
+++ setup.sh
@@ -0,0 +1,4 @@
+mkdir -p ~/.emacs.d
+
+ln -s -f init.el ~/.emacs.d/init.el
+ln -s -f config.org ~/.emacs.d/config.org
