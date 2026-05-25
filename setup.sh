2fa9cc8748ab86eaed79f5232b0ae750e8a6f877
mrprofessor <rudra.kar@icloud.com>
2026-05-26 02:09:50 +1200

    Update symlink script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 mkdir -p ~/.emacs.d
 
-ln -s ~/emacs.d/init.el ~/.emacs.d/init.el
-ln -s  ~/emacs.d/config.org ~/.emacs.d/config.org
+ln -s init.el ~/.emacs.d/init.el
+ln -s  config.org ~/.emacs.d/config.org
