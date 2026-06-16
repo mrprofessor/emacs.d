33ac3bef24664060b170a23add0b37eff229b29f
mrprofessor <rudra.kar@icloud.com>
2026-06-16 16:10:19 +1200

    Load vendored tomelr instead of flaky :vc checkout
    package-vc :rev :newest fails with 'Empty checkout for tomelr'. The
    repo already vendors packages/tomelr.el, so load it via :load-path and
    link packages/ into ~/.emacs.d in setup.sh.

--- setup.sh
+++ setup.sh
@@ -6,3 +6,4 @@ repo="$(cd "$(dirname "$0")" && pwd)"
 ln -sf "$repo/early-init.el" ~/.emacs.d/early-init.el
 ln -sf "$repo/init.el" ~/.emacs.d/init.el
 ln -sf "$repo/config.org" ~/.emacs.d/config.org
+ln -sf "$repo/packages" ~/.emacs.d/packages
